46fdbe3ee409aa2d6d5810bed7980998bc2a581b
leocarlos-dias <leocsdias@hotmail.com>
2023-07-07 00:17:06 -0300

    test: add test of rule for course entity

--- src/domain/entities/courses/course-entity.spec.ts
+++ src/domain/entities/courses/course-entity.spec.ts
@@ -1,3 +1,4 @@
+import { Subject } from "../subjects/subject-entity";
 import { Course } from "./course-entity";
 
 describe('Course Entity', () => {
@@ -16,7 +17,7 @@ describe('Course Entity', () => {
     expect(courseEntity).toBeInstanceOf(Course);
     expect(courseEntity).toMatchObject(course);
     expect(courseEntity.createdAt).toBeInstanceOf(Date);
-    expect(courseEntity.updatedAt).toBeInstanceOf(Date);      
+    expect(courseEntity.updatedAt).toBeInstanceOf(Date);
   });
 
   it('should update a course', () => {
@@ -38,5 +39,96 @@ describe('Course Entity', () => {
     });
     expect(courseEntity.createdAt).toBeInstanceOf(Date);
     expect(courseEntity.updatedAt).toBeInstanceOf(Date);
+  });
+
+  it("should add subject to course", () => {
+    // Arrange
+    const course = {
+      name: 'Course 1',
+      description: 'Description 1',
+    };
+
+    const subject = {
+      name: 'Subject 1',
+      workLoad: 60,
+    };
+
+    const courseEntity = Course.create(course);
+    const subjectEntity = Subject.create(subject);
+
+    // Act
+    courseEntity.addSubject(subjectEntity);
+
+    // Assert
+    expect(courseEntity.subjects).toHaveLength(1);
+    expect(courseEntity.subjects[0]).toBeInstanceOf(Subject);
+    expect(courseEntity.subjects[0]).toMatchObject(subject);
+  });
+
+  it("should not add subject to course if subject already exists", () => {
+    // Arrange
+    const course = {
+      name: 'Course 1',
+      description: 'Description 1',
+    };
+
+    const subject = {
+      name: 'Subject 1',
+      workLoad: 60,
+    };
+
+    const courseEntity = Course.create(course);
+    const subjectEntity = Subject.create(subject);
+    courseEntity.addSubject(subjectEntity);
+
+    // Act & Assert
+    expect(() => courseEntity.addSubject(subjectEntity)).toThrowError("Subject already exists");
+  });
+
+  it("should remove subject from course", () => {
+    // Arrange
+    const course = {
+      name: 'Course 1',
+      description: 'Description 1',
+    };
+
+    const subject = {
+      name: 'Subject 1',
+      workLoad: 60,
+    };
+
+    const courseEntity = Course.create(course);
+    const subjectEntity = Subject.create(subject);
+
+    // Act
+    courseEntity.addSubject(subjectEntity);
+
+    // Assert
+    expect(courseEntity.subjects).toHaveLength(1);
+
+    // Act
+    courseEntity.removeSubject(subjectEntity);
+
+    // Assert
+    expect(courseEntity.subjects).toHaveLength(0);
+  });
+
+  it("should not remove subject from course if subject does not exists", () => {
+    // Arrange
+    const course = {
+      name: 'Course 1',
+      description: 'Description 1',
+    };
+
+    const subject = {
+      name: 'Subject 1',
+      workLoad: 60,
+    };
+
+    const courseEntity = Course.create(course);
+    const subjectEntity = Subject.create(subject);
+
+    // Act & Assert
+    expect(() => courseEntity.removeSubject(subjectEntity)).toThrowError("Subject does not exists");
   });
 });
