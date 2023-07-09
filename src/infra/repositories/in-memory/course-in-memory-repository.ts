ee2b391b098697be5bc45d05eb674b3dca684fb6
leocarlos-dias <leocsdias@hotmail.com>
2023-07-08 21:45:59 -0300

    feat: add course repository in memory

---
+++ src/infra/repositories/in-memory/course-in-memory-repository.ts
@@ -0,0 +1,18 @@
+import { CourseDTO } from "../../../app/dtos/course-dto";
+import { CourseRepository } from "../../../domain/repotisories/course-repository";
+
+export class CourseInMemoryRepository implements CourseRepository {
+
+  constructor(private readonly courses: CourseDTO[] = []) {}
+
+  async findById(id: string): Promise<CourseDTO | undefined> {
+    return this.courses.find((course) => course.id === id);
+  }
+  async create(course: CourseDTO): Promise<void> {
+    this.courses.push(course);
+  }
+  async save(course: CourseDTO): Promise<void> {
+    const courseIndex = this.courses.findIndex((u) => u.id === course.id);
+    this.courses[courseIndex] = course;
+  }
+}
