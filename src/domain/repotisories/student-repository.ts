063b5c6e126fa0006e0568908cdb46222503a246
leocarlos-dias <leocsdias@hotmail.com>
2023-07-08 21:38:44 -0300

    feat: add method find by id in user repository

--- src/domain/repotisories/student-repository.ts
+++ src/domain/repotisories/student-repository.ts
@@ -2,6 +2,7 @@ import { StudentProps } from "../entities/students/student-entity";
 
 export interface StudentRepository {
   findByEmail(email: string): Promise<StudentProps | undefined>;
+  findById(id: string): Promise<StudentProps | undefined>;
   create(student: StudentProps): Promise<void>;
   save(student: StudentProps): Promise<void>;
 }
